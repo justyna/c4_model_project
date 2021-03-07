8ad3b882484881d5e591a028be6abedda71ad610
justyna <zielonydomek@gmail.com>
2021-03-07 16:24:20 +0100

    Add configuration json

---
+++ ModelC4Config.cs
@@ -0,0 +1,9 @@
+namespace modelc4_project
+{
+    internal class ModelC4Config
+    {
+        public int WorkspaceId {get; set;}
+        public string ApiKey {get; set;}
+        public string ApiSecret {get; set;}
+    }
+}
